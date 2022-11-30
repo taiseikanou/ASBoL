bbcfddd6ea43e70dcd334bdabaf73e001f326484
EC2 Default User <dachengjiana76@gmail.com>
2022-11-30 00:44:06 +0000

    表示の変更

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -16,7 +16,7 @@ class Public::PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to admin_posts_path
+    redirect_to posts_path
   end
 
   def edit
